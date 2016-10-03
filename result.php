9ad18c7b83943e1bd16a98452755888420c9066f
Shivam srivastava <er1shivam@gmail.com>
2016-10-04 00:42:23 +0530

    Added new styling to the result page

--- result.php
+++ result.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="stylesheet/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet/style1.css"/>
 </head>
 <body>
-    
+<div class="ful">
+     <div class="row">
+        <div class="col-md-1">
+        <h3>DHUND </h3>
+        </div>
+        <div class="col-md-9">
+        <form action="result.php" method="get">
+        <input name="user_keyword" class="search-box" type="text" size="60" />
+        <input name="result" type="submit" value="Search" class="btn" />
+        </form>
+        </div>
+      </div>
+      <main>
+      <p>
+      this si fsjfls
+      </p>
+      <p>
+      jksflsdf
+      </p>
+
+
+
+</main>
+
+</div>
 </body>
 </html>
